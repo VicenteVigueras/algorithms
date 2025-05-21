b323f2ebff9573de3544f09aa1ad823489771e87
Vicente Vigueras <vicente.viguerassilva@gmail.com>
2025-05-20 22:19:34 -0400

    feat: add TwoSum solution

---
+++ algos/TwoSum.java
@@ -0,0 +1,18 @@
+package algos;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+ public static int[] TwoSum(int[] nums, int target) {
+    Map<Integer, Integer> complementByIndex = new HashMap<>();
+    for(int i =0; i < nums.length; i++) {
+        int complement = target - nums[i];
+        if(complementByIndex.containsKey(complement)) {
+            return new int[] {complementByIndex.get(complement), i};
+        }
+        complementByIndex.put(nums[i], i);
+    }
+    return new int[] {};
+ }   
+}
