7edcffedf9425c4670a7c4e56ff48807ce38b8f8
Vicente Vigueras <vicente.viguerassilva@gmail.com>
2025-05-21 10:17:03 -0400

    feat: hasDuplicate() Set method

---
+++ algos/HasDuplicate.java
@@ -0,0 +1,15 @@
+package algos;
+
+import java.util.HashSet;
+
+public class HasDuplicate {
+    public boolean hasDuplicate(int[] nums) {
+     var seen = new HashSet<Integer>();
+     for(int i = 0; i < nums.length; i++) {
+        if(!seen.add(nums[i])) {
+          return true;
+        }
+     }
+     return false;
+    }
+}
