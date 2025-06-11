8c6175d6609ee18cc9f592dee507d325c3a918ab
VicenteVigueras <vicente.viguerassilva@gmail.com>
2025-06-11 14:50:50 -0400

    feat: leetcode 20

---
+++ IsValid.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValid(String s) {
+        var stack = new Stack<Character>();
+        if (s.length() == 0 || s.length() == 1) {
+            return false;
+        }
+        for (char current : s.toCharArray()) {
+            if (current == '(' || current == '[' || current == '{') {
+                stack.push(current);
+            } else {
+                if (stack.isEmpty())
+                    return false;
+                if (current == '}' && stack.peek() == '{' ||
+                        current == ']' && stack.peek() == '[' ||
+                        current == ')' && stack.peek() == '(') {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
