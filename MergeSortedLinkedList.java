d16e7f93122d466072fe6d8414cd1cf994175714
VicenteVigueras <vicente.viguerassilva@gmail.com>
2025-06-11 21:51:23 -0400

    chore: current progress on leetcode 21

--- MergeSortedLinkedList.java
+++ MergeSortedLinkedList.java
@@ -17,18 +17,24 @@ class Solution {
             return list1;
         }
         ListNode ans = new ListNode();
-        while (list1.next != null && list2.next != null) {
-            System.out.println(list1.val + " + " + list2.val);
+        ListNode tail = ans;
+        
+        while (list1 != null && list2 != null) {
+            // System.out.println( "[" + list1.val + " , " + list2.val + "]");
             if(list1.val == list2.val) {
+                System.out.println(list1.val);
+                System.out.println(list2.val);
                 ans = list1;
-                ans.next = list2;
             }
-            else if (list1.val < list2.val) {
+            if (list1.val < list2.val) {
+                System.out.println(list1.val);
+                System.out.println(list2.val);
                 ans = list1;
-                ans.next = list2;
-            } else if (list1.val > list2.val) {
+            }
+            if (list1.val > list2.val) {
+                System.out.println(list2.val);
+                System.out.println(list1.val);
                 ans = list2;
-                ans.next = list1;
             } 
             list1 = list1.next;
             list2 = list2.next;
