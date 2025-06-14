3e69ff536e4b5f1175189c9b9a2e275ae0ddc06d
VicenteVigueras <vicente.viguerassilva@gmail.com>
2025-06-13 22:16:29 -0400

    chore: add comment structure to missing number

--- MissingNumber.java
+++ MissingNumber.java
@@ -1,3 +1,8 @@
+    /* 
+    Problem: Leetcode 268; Difficulty Easy
+    Time-complexity: O(log n)
+    Space-complexity: O(n * log(n)) 
+    */ 
 public class MissingNumber {
     public int missingNumber(int[] nums) {
         Arrays.sort(nums);
@@ -9,3 +14,8 @@ public class MissingNumber {
         return nums.length;
     }
 }
+    /*
+    Can it be better?
+    Yes, this is generally solved in O(n) time and O(1) space
+    TODO: Figure out how to improve it
+    */
